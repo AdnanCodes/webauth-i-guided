064b019463b9d74683863f831d4bb9e71d1d206d
Adnan <adnan.chowdhury@outlook.com>
2019-09-16 12:38:48 -0500

    middleware for validating creds

--- index.js
+++ index.js
@@ -60,5 +60,29 @@ server.get("/hash", (req, res) => {
   res.send(`the hash for ${name} is ${hash}`);
 });
 
+function ValidateCreds(req, res, next) {
+  // we'll read the username and password from headers
+  // the client is responsible for setting those headers
+  const { username, password } = req.headers;
+
+  // no point on querying the database if the headers are not present
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Unexpected error" });
+      });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
